cab9ee88d67ba1c739fb8fb6c04984d096917d57
Kareem James <kareemdjames@gmail.com>
2018-08-04 15:55:20 -0400

    created container to wrap components

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { TodoList } from './components';
+import { createStore} from 'redux';
+import reducer from './reducer';
+
+const store = createStore(reducer);
+
+render(
+  <TodoList todos={store.getState()} />,
+  document.getElementById('app')
+);
